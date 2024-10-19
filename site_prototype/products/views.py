e001299ddac2183f6d794da3cee10ba58086d9df
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-10-19 15:30:18 +0400

    created views for products and categories

--- site_prototype/products/views.py
+++ site_prototype/products/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product, Category
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "products/product_list.html"
+    context_object_name = "products"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+    context_object_name = "product"
+
+    def get_object(self):
+        return Product.objects.get(slug=self.kwargs["slug"])
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = "products/category_list.html"
+    context_object_name = "categories"
+
+    def get_queryset(self):
+        return Category.objects.all()
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = "products/category_detail.html"
+    context_object_name = "category"
+
+    def get_object(self):
+        return Category.objects.get(slug=self.kwargs["slug"])
