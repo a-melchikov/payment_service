640b4c76592712db13ece859a6fedfa189bcc6e0
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-10-19 13:46:23 +0400

    create products app and add Product and Category models

---
+++ site_prototype/products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to="products/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
