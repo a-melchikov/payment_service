6e91b0f5593108c9f95cc11f150bd9cb1e87c458
Alexander Korepanov <alexander.korepanov@ardecs.com>
2024-10-20 10:49:55 +0400

    change transaction tables

--- backend/src/main/resources/database/create-table/failed_transactions.sql
+++ backend/src/main/resources/database/create-table/failed_transactions.sql
@@ -1,10 +1,9 @@
 CREATE TABLE failed_transactions (
     transaction_id SERIAL PRIMARY KEY,
-    card_id INT REFERENCES cards(card_id),
+    payment_identifier VARCHAR(20),
     amount DECIMAL(10, 2) NOT NULL,
     transaction_date TIMESTAMP NOT NULL,
     user_id INT REFERENCES users(user_id),
-    description TEXT,
     error_message TEXT NOT NULL,
     payment_method VARCHAR(20) CHECK (payment_method IN ('card', 'yoomoney'))
 );
