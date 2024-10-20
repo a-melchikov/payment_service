a04324762ae3cc4d5a53a9182d785531bbf39168
Alexander Korepanov <alexander.korepanov@ardecs.com>
2024-10-20 13:42:25 +0400

    add swagger to backend

--- backend/src/main/java/com/paymentservice/backend/controller/BasePaymentProcessingController.java
+++ backend/src/main/java/com/paymentservice/backend/controller/BasePaymentProcessingController.java
@@ -7,6 +7,8 @@ import com.paymentservice.backend.dto.BankCardPaymentRequest;
 import com.paymentservice.backend.dto.BankCardPaymentResponse;
 import com.paymentservice.backend.service.BankCardProcessingService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +17,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/payments")
+@Tag(name = "Payment Service API", description = "API для проведения оплаты различными способами")
 public class BasePaymentProcessingController {
     private final BankCardProcessingService bankProcessingService;
     
     @PostMapping("/bankcard")
+    @Operation(summary = "Осуществить платеж по реквизитам банковской карты")
     public BankCardPaymentResponse postMethodName(@RequestBody BankCardPaymentRequest bankCardPaymentRequest) {
         return bankProcessingService.processBankPayment(bankCardPaymentRequest);
     }
