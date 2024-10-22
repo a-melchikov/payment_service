26414c08878477857d7fe84e977c97db12b00c5a
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-10-22 18:15:32 +0400

    update base.html, style.css

--- site_prototype/users/urls.py
+++ site_prototype/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("profile/", LogoutView.as_view(), name="profile"),
 ]
