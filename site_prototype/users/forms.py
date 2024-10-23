ee8c42a38330084b703b377f9e33d89ac4b8e626
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-10-23 14:25:00 +0400

    update register form

--- site_prototype/users/forms.py
+++ site_prototype/users/forms.py
@@ -8,6 +8,29 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ("username", "password1", "password2", "birth_date")
 
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Имя пользователя"}
+        ),
+        label="Имя пользователя",
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Пароль"}
+        ),
+        label="Пароль",
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Подтверждение пароля"}
+        ),
+        label="Подтверждение пароля",
+    )
+    birth_date = forms.DateField(
+        widget=forms.DateInput(attrs={"type": "date", "class": "form-control"}),
+        label="Дата рождения",
+    )
+
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(label="Имя пользователя")
