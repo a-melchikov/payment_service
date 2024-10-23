02e85de8f1809ab7937f4b2a7316f05d4c554c0b
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-10-23 14:13:01 +0400

    added password change functionality

--- site_prototype/users/urls.py
+++ site_prototype/users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import (
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    LogoutView,
+)
 
 from .views import UserProfileView, UserRegisterView, UserLoginView
 
@@ -8,4 +12,14 @@ urlpatterns = [
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("profile/", UserProfileView.as_view(), name="profile"),
+    path(
+        "password-change/",
+        PasswordChangeView.as_view(template_name="users/password_change.html"),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"),
+        name="password_change_done",
+    ),
 ]
