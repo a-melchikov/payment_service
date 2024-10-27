7cb996a3d90404c16fbd2f45a17745e78a20bf6b
dmitry chubukov <faiklem@mail.ru>
2024-10-27 16:24:28 +0400

    fix bugs

--- src/utils/isPastDate.ts
+++
@@ -1,8 +0,0 @@
-function isPastDate(date: string, currentDate: Date) {
-	let [month, year] = date.split("/");
-	const inputDate = new Date(parseInt(year) + 2000, parseInt(month) - 1);
-
-	return inputDate < currentDate;
-}
-
-export default isPastDate;
